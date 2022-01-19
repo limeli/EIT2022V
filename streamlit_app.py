09a5e627d35542edd3170dbd3853a77225787378
Gunnar G. Grotmol <gunnar.grotmol@gmail.com>
2022-01-19 14:19:42 +0100

    test save

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,9 @@
 import streamlit as st
 
 
-st.write("Hello Multiverse!")
-st.button("click me")
-
 def click_button():
     st.write("you clicked! YAY!")
+
+st.write("Hello Multiverse!")
+st.button("click me", on_click = click_button)
+
