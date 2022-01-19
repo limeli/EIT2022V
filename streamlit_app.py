fd388a073805491f5c8e42a180a8fa687f9c5f03
Gunnar G. Grotmol <gunnar.grotmol@gmail.com>
2022-01-19 15:22:42 +0100

    columns

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.header('Provotype: Model Explanation')
 def click_button():
     st.write("you clicked! YAY!")
 
-col1, col2 = st.columns(2)
+col1, col21, col22 = st.columns([3, 1, 2])
 with col1:
     st.write("Hello Multiverse!")
     st.image('./Feature_importance.png')
@@ -15,10 +15,8 @@ with col1:
     st.write("Fucking hell")
 
 
-with col2:
-    col21, col22 = st.columns([1,2])
-    with col21:
-        st.markdown("Pick a number")
-    with col22:
-        st.number_input(min_value=0, max_value=10)
+with col21:
+    st.markdown("Pick a number")
+with col22:
+    st.number_input(min_value=0, max_value=10)
 
