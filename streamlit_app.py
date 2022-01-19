b3f47e2b183c5d444f2d1fa1f62ea09ffb155acb
Gunnar G. Grotmol <gunnar.grotmol@gmail.com>
2022-01-19 15:37:56 +0100

    columns

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,5 +16,13 @@ with col1:
 
 
 with col2:
-    st.number_input("Pick a number",min_value=0, max_value=10)
+    st.selectbox("Gender", ["Male", "Female", "Other"])
+    st.slider("Age", 18, 100)
+    st.checkbox("Same address as parent(s)")
+    st.selectbox("Citizenship", ["Norwegian", "Other"])
+    st.slider("University Credits", 0, 600)
+    st.number_input("Annual income", min_value=0, max_value=1000000)
+    st.number_input("Value of personal assets", min_value=-1000000, max_value=1000000)
+    st.checkbox("Mental disorder")
+    st.number_input("How many times cried this week")
 
