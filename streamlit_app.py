9bb4924672357b3b05a76e2156b9737c65a25f6c
Gunnar G. Grotmol <gunnar.grotmol@gmail.com>
2022-01-20 10:11:24 +0100

    wee

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,11 +22,8 @@ with col1:
 
 with col2:
     st.header('Model Calculator')
-    colA, colB = st.beta_columns([1,1])
-    with colA:
-        st.number_input("Age", 18, 100)
-    with colB:
-        st.selectbox("Sex", ["Male", "Female", "Other"])
+    st.number_input("Age", 18, 100)
+    st.selectbox("Sex", ["Male", "Female", "Other"])
     st.selectbox("Citizenship", ["Norwegian", "Other"])
     st.multiselect("Family Status", ["Parent", "Child"])
     st.number_input("Postal Code", 0, 9999)
