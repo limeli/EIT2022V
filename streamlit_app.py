2379348fc156b94fb5ec7acdb1821b7bba1c37c6
Gunnar G. Grotmol <gunnar.grotmol@gmail.com>
2022-01-20 09:10:04 +0100

    tried setting bg

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 page_bg_img = '''
 <style>
 body {
-background-image: url("https://ibb.co/F746J9H");
+background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
 background-size: cover;
 }
 </style>
